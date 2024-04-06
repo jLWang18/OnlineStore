ea0058b5693d44a2f65deeeaa38f876504c93068
Jason Wangadi <jasonlukas.wangadi8@gmail.com>
2024-04-06 11:00:39 -0700

    seperate controller and services for dispplaying customers and adding customer for swaggerUI

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,20 +1,20 @@
-from flask import Flask, jsonify, render_template, request, redirect, url_for, json
-import pyodbc
-from datetime import datetime
+from flask import Flask, jsonify, render_template, request, redirect, url_for
 from flask_swagger_ui import get_swaggerui_blueprint
+from datetime import datetime
+import pyodbc
+import mywebservice
+import myswaggerservice
 
-# Import a custom UIService class for API Service call
-from webservice import MyWebService
-
-# Set up flask application
+# set up flask application
 app = Flask(__name__)
-webservice = MyWebService()
+
+
 
 ### Swagger specific ###
 SWAGGER_URL = '/swagger' 
 API_URL = '/static/swagger/swagger.json'
 
-# Call factory function to create the Swagger UI blueprint
+# call factory function to create the Swagger UI blueprint
 swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,  
     API_URL,
@@ -26,7 +26,7 @@ swaggerui_blueprint = get_swaggerui_blueprint(
 app.register_blueprint(swaggerui_blueprint, url_prefix = SWAGGER_URL)
 ### End Swagger specific ###
 
-# Define route for rendering the homepage
+# define route for rendering the homepage
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
@@ -37,339 +37,135 @@ def signin():
     # redirect to endpoint customer_info
     return redirect(url_for('customer_info_ui'))
 
-# Define route for rendering the customer_info page
+# define route for rendering the customer_info page
 @app.route("/customer_info", methods=['GET'])
 def customer_info_ui():
     return render_template('customer_info.html')
 
-# Format phone input
-def format_phone():
-    #Convert phone input in from (XXX)-(XXX)-(XXXX) to XXXXXXXXX format
+# format phone input
+def format_phone(phone):
+    # convert phone input in from (XXX)-(XXX)-(XXXX) to XXXXXXXXX format
     replacements = [('(', ''), ('-', ''), (')', '')]
     
     for char, replacement in replacements:
         if char in phone:
             phone = phone.replace(char, replacement)
     
-    #make sure there is no spaces         
+    # make sure there is no spaces         
     phone = phone.replace(' ', '') 
     return phone
 
-# Define Flask API routes for customer_info HTML form
+# define Flask API routes for Web UI to add customer to the database
 @app.route('/submit_customer', methods=['POST'])
 def submit_customer():
-    # initiates database connection and cursor
-    conn = None
-    cursor = None
+        
+    # instantiate MyWebService class
+    webservice = mywebservice.MyWebService()
     
-    if request.method == 'POST':
-        # Establish connection to the database
-        conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};' 'Server=(localdb)\MSSQLLocalDB;' 'Database=account_receivable;' 'Trusted_Connection=yes;')
-        # Create a cursor object
-        cursor = conn.cursor()
-        
-        #get inputs
-        first_name = request.form['fname']
-        last_name = request.form['lname']
-        email = request.form['email']
-        phone = request.form['phone-number']
-        
-        #default params
-        created_date = datetime.now()
-        modified_date = None
-        
-        #format phone input
-        phone = format_phone(phone)
-        
-        #add customer email
-        try: 
-            #call the method from myWebService class 
-            webservice.add_customer_email(conn, cursor, first_name, last_name, email, created_date, modified_date)
-        except Exception as e:
-            error_message = 'An error occured while adding customer email: ' + str(e)
-            return error_message
-    else:
-        return "Invalid Request"        
+    # get user input
+    first_name = request.form['fname']
+    last_name = request.form['lname']
+    email = request.form['email']
+    phone = request.form['phone-number']
+    
+    # default params
+    created_date = datetime.now()
+    modified_date = None
+    
+    # format phone input
+    phone = format_phone(phone)
+    
+    # call the method from myWebService class 
+    message = webservice.add_customer(first_name, last_name, email, phone, created_date, modified_date)
+    return message        
          
 
-# Define Flask API routes for SwaggerUI
+# define Flask API routes for SwaggerUI to display all customers
 @app.route('/api/customer-info/get', methods=['GET'])
-def get_details():
-    # initiates database connection and cursor
-    conn = None
-    cursor = None
-    try:
-        #Establish connection to the database
-        conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};' 'Server=(localdb)\MSSQLLocalDB;' 'Database=account_receivable;' 'Trusted_Connection=yes;')
-        # Create a cursor object
-        cursor = conn.cursor()
-        sql_get_all_customer_details = "EXEC spCustomerTrial_GetAll"
-        cursor.execute(sql_get_all_customer_details)
-        
-        # Fetch all rows from the cursor
-        rows = cursor.fetchall()
-        
-        # Create a list to store customer details
-        customer_details = []
-        
-        #Iterate through rows of customer details
-        for row in rows:
-            customer_detail = {
-                'customer_id': row.pk_customer_id,   
-                'first_name': row.first_name,
-                'last_name': row.last_name,
-                'email_address': row.email_address,
-                'mobile_phone': row.mobile_phone,
-                'created_date': row.created_date,
-                'modified_date': row.modified_date
-            }
-            customer_details.append(customer_detail)
-        
-        conn.commit()
-        return jsonify({'message': 'All Customer\'s records displayed successfully', 'data': customer_details}), 200
+def get_all_customers():
     
+    try:    
+        # instantiate MySwaggerService class
+        swaggerservice = myswaggerservice.MySwaggerService()
+        
+        # display customer info by calling the method in MySwaggerService class
+        customer_details = swaggerservice.display_all_customers()
+        
+        return jsonify({'message': 'All customer\'s records displayed successfully', 'data': customer_details}), 200
     except Exception as e:
         error_message = 'There was an issue displaying customer\'s records' + str(e)
-        return jsonify({'error': error_message}), 400
-    
-    finally:
-        #close cursor and connection
-        if cursor:
-            cursor.close()
-        else:
-            conn.close()
-        
+        return jsonify({'error': error_message}), 400          
+
+       
 @app.route('/api/customer-info/<int:customer_id>', methods=['GET'])
-def get_customer_detail(customer_id):
-    #initiates database connection and cursor
-    conn = None
-    cursor = None  
-    #print("The customer's id arg input is " + str(customer_id))
-    try:
-        # Check the request Content-Type is application/x-www-form-urlencoded
-        if request.headers.get('Content-Type', ''):
-            return jsonify({'error': 'Unsupported Media Type.  Please send data with Content-Type: application/x-www-form-urlencoded'}), 415
-        
-        print("Before gettting the customer_id")
-        #Establish connection to the database
-        conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};' 'Server=(localdb)\MSSQLLocalDB;' 'Database=account_receivable;' 'Trusted_Connection=yes;')
-        # Create a cursor object
-        cursor = conn.cursor()
-        
+def get_customer_detail(customer_id): 
     
-            
-        try: 
-            #Execute query
-            sql_customer_getbyid = "EXEC spCustomerTrial_GetById ?"
-            cursor.execute(sql_customer_getbyid, (customer_id))
-            
-            #get customer data
-            data = cursor.fetchone()
-                
-            customer_detail = {
-                'customer_id': data.pk_customer_id,   
-                'first_name': data.first_name,
-                'last_name': data.last_name,
-                'email_address': data.email_address,
-                'mobile_phone': data.mobile_phone,
-                'created_date': data.created_date,
-                'modified_date': data.modified_date
-                }
-                
-            conn.commit()
-            return jsonify({'message': 'All Customer\'s records displayed successfully', 'data': customer_detail}), 200
-        except Exception:
-                error_message = 'Customer ID is not found'
-                return jsonify({'error': error_message}), 400
-            
-    finally:
-        # close cursor and connection
-        if cursor:
-            cursor.close()
-        if conn:
-            conn.close()
+    # instantiate swagger service
+    swaggerservice = myswaggerservice.MySwaggerService()
+    # get a customer info
+    customer_detail = swaggerservice.display_customer(customer_id)
+    
+    # if there is a customer, display it
+    if(customer_detail is not None):
+          return jsonify({'message': 'All Customer\'s records displayed successfully', 'data': customer_detail}), 200
+    else:
+        return jsonify({'error': 'Customer ID is not found'})
+
 
 @app.route('/api/customer-info/addCustomerPhone', methods=['POST'])
 def customer_phone():
     
-    # initiates database connection and cursor
-    conn = None
-    cursor = None
-
-    try:
-        # Check the request Content-Type is application/x-www-form-urlencoded
-        if request.headers.get('Content-Type', ''):
-            return jsonify({'error': 'Unsupported Media Type.  Please send data with Content-Type: application/x-www-form-urlencoded'}), 415
-        
-        # Establish connection to the database
-        conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};' 'Server=(localdb)\MSSQLLocalDB;' 'Database=account_receivable;' 'Trusted_Connection=yes;')
-        # Create a cursor object
-        cursor = conn.cursor()
-
-        # Get phone number from query parameter
-        phone_number = request.args.get('phone_number')
-
-        if phone_number is not None:
-            # default params
-            created_date = datetime.now()
-            modified_date = None
-
-            # Insert customer's phone number
-            try:
-                sql_phone_insert_query = "EXEC spMobilePhoneTrial_InsertAll ?, ?, ?"
-                cursor.execute(sql_phone_insert_query, (phone_number, created_date, modified_date))
-                conn.commit()
-                return jsonify({'message': 'Customer\'s phone number added successfully'}), 200
-            except Exception as e:
-                error_message = "There was an issue adding customer's phone number: " + str(e)
-                return jsonify({'error': error_message}), 500
-        else:
-            return jsonify({'error': 'phone number parameter is missing'}), 400
-
-    finally:
-        # close cursor and connection
-        if cursor:
-            cursor.close()
-        if conn:
-            conn.close()
-
+    # Check the request Content-Type is application/x-www-form-urlencoded
+    if request.headers.get('Content-Type', ''):
+        return jsonify({'error': 'Unsupported Media Type.  Please send data with Content-Type: application/x-www-form-urlencoded'}), 415
+    
+    # get phone number from query parameter
+    phone_number = request.args.get('phone_number')  
+    
+    # instantiate swagger service 
+    swaggerservice = myswaggerservice.MySwaggerService()
+    
+    message = swaggerservice.add_customer_phone(phone_number)
+    
+    return message
+   
 @app.route('/api/customer-info/addCustomerEmail', methods=['POST'])
 def customer_email():
+    # check the request Content-Type is application/x-www-form-urlencoded
+    if request.headers.get('Content-Type', ''):
+        return jsonify({'error': 'Unsupported Media Type.  Please send data with Content-Type: application/x-www-form-urlencoded'}), 415
 
-    # initiates database connection and cursor
-    conn = None
-    cursor = None
+    # get email from query parameter
+    email = request.args.get('email')
+    
+    # instantiate swagger service
+    swaggerservice = myswaggerservice.MySwaggerService()
+    
+    # add email
+    message = swaggerservice.add_customer_email(email)
 
-    try:
-        # Check the request Content-Type is application/x-www-form-urlencoded
-        if request.headers.get('Content-Type', ''):
-            return jsonify({'error': 'Unsupported Media Type.  Please send data with Content-Type: application/x-www-form-urlencoded'}), 415
+    return message
 
-        # Establish connection to the database
-        conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};' 'Server=(localdb)\MSSQLLocalDB;' 'Database=account_receivable;' 'Trusted_Connection=yes;')
-        # Create a cursor object
-        cursor = conn.cursor()
-
-        # Get email from query parameter
-        email = request.args.get('email')
-
-        if email is not None:
-            # default params
-            created_date = datetime.now()
-            modified_date = None
-            
-            # Insert customer's email 
-            try:
-                sql_email_insert_query = "EXEC spEmailAddressTrial_InsertAll ?, ?, ?"
-                cursor.execute(sql_email_insert_query, (email, created_date, modified_date))
-                conn.commit()
-                # return a valid response
-                return jsonify({'message': 'Customer\'s email added successfully'}), 200
-            except Exception as e:
-                error_message = "There was an issue adding customer's email address: " + str(e)
-                return jsonify({'error': error_message}), 500
-        else:
-            return jsonify({'error': 'email parameter is missing'}), 400
-        
-    finally:
-        # close cursor and connection
-        if cursor:
-            cursor.close()
-        if conn:
-            conn.close()
-
-
-@app.route('/customer-info/addCustomer', methods=['POST'])
+@app.route('/api/customer-info/addCustomer', methods=['POST'])
 def customer_info():
+    
+    # check the request Content-Type is application/x-www-form-urlencoded
+    if request.headers.get('Content-Type', ''):
+        return jsonify({'error': 'Unsupported Media Type.  Please send data with Content-Type: application/x-www-form-urlencoded'}), 415
 
-    # initiates database connection and cursor
-    conn = None
-    cursor = None
-
-    try:
-        # Check the request Content-Type is application/x-www-form-urlencoded
-        if request.headers.get('Content-Type', ''):
-            return jsonify({'error': 'Unsupported Media Type.  Please send data with Content-Type: application/x-www-form-urlencoded'}), 415
-
-        
-        # Establish connection to the database
-        conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};' 'Server=(localdb)\MSSQLLocalDB;' 'Database=account_receivable;' 'Trusted_Connection=yes;')
-        # Create a cursor object
-        cursor = conn.cursor()
-
-        # Get input from query parameteter
-        first_name = request.args.get('first_name')
-        last_name = request.args.get('last_name')
-        email_id = request.args.get('email')
-        phone_number_id = request.args.get('phone_number')
-
-        #get the email from the database
-        sql_customer_get_email_byid_query = "EXEC spEmailAddressTrial_GetById ?"
-        cursor.execute(sql_customer_get_email_byid_query, (email_id))
-        email = cursor.fetchone()
-
-        #get the phone_number from the database
-        sql_customer_get_phone_byid_query = "EXEC spPhoneNumberTrial_GetById ?"
-        cursor.execute(sql_customer_get_phone_byid_query, (phone_number_id))
-        phone = cursor.fetchone()
-
-        #if the email is not found, return jsonify error
-        if email is None:
-            return jsonify({'error': f'Customer\'s email by email_id {email_id} is not found'}), 400
-        elif phone is None:
-            return jsonify({'error': f'Customer\'s phone number by phone_number_id {phone_number_id} is not found'}), 400
-        
-        if first_name and last_name is not None:
-            # default params
-            created_date = datetime.now()
-            modified_date = None
-        
-            # Insert customer's name
-            try:
-                sql_customer_insert_query = "EXEC spCustomerTrial_InsertAll ?, ?, ?, ?, ?, ?"
-                cursor.execute(sql_customer_insert_query, (first_name, last_name, email_id, phone_number_id, created_date, modified_date))
-                conn.commit() 
-                return jsonify({'message': 'Customer\'s profile added successfully'}), 200
-            except Exception as e:
-                error_message = "There was an issue adding customer's info: " + str(e)
-                return jsonify({'error': error_message}), 500
-        else:
-            return jsonify({'error': 'one of the parameter is missing'}), 400
-        
-    finally:
-        # close cursor and connection
-        if cursor:
-            cursor.close()
-        if conn:
-            conn.close()
-
-@app.route("/customer-info/get", methods=['GET'])
-def get_customer_info():
-        try:
-            # Establish connection to the database
-            conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};' 'Server=(localdb)\MSSQLLocalDB;' 'Database=account_receivable;' 'Trusted_Connection=yes;')
-            # Create a cursor object
-            cursor = conn.cursor()
-            
-            # Fetch customers data
-            cursor.execute("EXEC spCustomerTrial_GetAll")
-            customers = cursor.fetchall()
-
-            # if there are customers data
-            if customers:
-                # Return the customers as JSON
-                return jsonify(customers)
-            else:
-                return "No customers found", 404
-            
-        except Exception as e:
-            error_message = "There was an issue fetching customer data: " + str(e)
-            return error_message
-        
-        finally:
-        # close cursor and connection
-            cursor.close()
-            conn.close()
+    # get input from query parameteter
+    first_name = request.args.get('first_name')
+    last_name = request.args.get('last_name')
+    email_id = request.args.get('email')
+    phone_number_id = request.args.get('phone_number')
+    
+    # instantiate swaggerservice
+    swaggerservice = myswaggerservice.MySwaggerService()
+    
+    # add customer
+    message = swaggerservice.add_customer(first_name, last_name, email_id, phone_number_id)
+    
+    return message
 
 if __name__== "__main__":
     app.run(debug=True)
--- flask_app/mywebservice.py
+++ flask_app/mywebservice.py
@@ -1,87 +1,154 @@
+import pyodbc
 class MyWebService:
+    # static  SQL connection
+    conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};' 'Server=(localdb)\MSSQLLocalDB;' 'Database=account_receivable;' 'Trusted_Connection=yes;')
     
-    #add customer email
-    def add_customer_email(conn, cursor, email, created_date, modified_date):
+    # close sql cursor & Connection
+    def close_sql(self, cursor):
+       # if cursor open, close it
+        if cursor:
+            cursor.close()
+        else:
+         self.conn.close()
+            
+    # add customer email
+    def add_customer_email(self, email, created_date, modified_date):
         try:
-          #First add email to the email table
+            # first add email to the email table
             sql_email_insert_ui_query = "EXEC spEmailAddressTrial_InsertAll ?, ?, ?"
+            
+            # create a cursor object
+            cursor = self.conn.cursor()
+            
             cursor.execute(sql_email_insert_ui_query, (email, created_date, modified_date))
-            conn.commit()
+            self.conn.commit()
+            
+            # close cursor and connection
+            # self.close_sql(cursor)
+            
             return True
-        except Exception as e:
+        except Exception:
+            # close cursor and connection
+            self.close_sql(cursor)
+            
             return False
     
-    #add customer phone
-    def add_customer_phone(conn, cursor, phone, created_date, modified_date):
+    # add customer phone
+    def add_customer_phone(self, phone, created_date, modified_date):
         try:
-            #second add phone to the phone table
+            # second add phone to the phone table
             sql_phone_insert_ui_query = "EXEC spMobilePhoneTrial_InsertAll ?, ?, ?"
+            
+            # create cursor object
+            cursor = self.conn.cursor()
+            
             cursor.execute(sql_phone_insert_ui_query, (phone, created_date, modified_date))
-            conn.commit()
+            self.conn.commit()
+            
             return True
-        except Exception as e:
+        except Exception:
+            # close cursor and connection
+            self.close_sql(cursor)
+            
             return False
     
-    #get email id from the database
-    def get_email_id(conn, cursor, email):
+    # get email id from the database
+    def get_email_id(self, email):
+        
         email_id = None
+        
         sql_customer_get_email_byid_query = "EXEC spEmailAddressTrial_GetIdByEmail ?"
+        # create cursor object
+        cursor = self.conn.cursor()
         cursor.execute(sql_customer_get_email_byid_query, (email))
         
         #fetch the row tuple
         result = cursor.fetchone()
         
-        #check if result is not None and is an integer
+        # check if result is not None and is an integer
         if result is not None and isinstance(result[0], int):
-            #assigned the first tuple value to email id
+            # assigned the first tuple value to email id
             email_id = result[0]
+            
             return email_id
         else:
+            # close cursor and connection
+            self.close_sql(cursor)
+            
             print("Error: email_id is not an integer or is None")
     
     #get phone id from the database
-    def get_phone_id(conn, cursor, phone):
+    def get_phone_id(self, phone):
+        
+        phone_id = None
+        
         sql_customer_get_phone_byid_query = "EXEC spMobilePhoneTrial_GetIdByPhone ?"
+        # create cursor object
+        cursor = self.conn.cursor()
         cursor.execute(sql_customer_get_phone_byid_query, (phone))
         
         #fetch the row tuple
         result = cursor.fetchone()
         
-        #check if result is not None and is an integer
+        # check if result is not None and is an integer
         if result is not None and isinstance(result[0], int):
-            #assigned the first tuple value to phone id
+            # assigned the first tuple value to phone id
             phone_id = result[0]
+            
             return phone_id
         else:
-            print("Error: phone_id is not an integer or None")
+            # close cursor and connection
+            self.close_sql(cursor)
             
-    #submit customer info logic
-    def add_customer(conn, cursor, first_name, last_name, email, phone, created_date, modified_date):
+            print("Error: phone_id is not an integer or None")
+    
+    # add customer's first and last names
+    def add_customer_name(self, first_name, last_name, email_id, phone_id, created_date, modified_date):
+         try:
+             sql_customer_insert_ui_query = "EXEC spCustomerTrial_InsertAll ?, ?, ?, ?, ?, ?"
+             
+             # create cursor obj
+             cursor = self.conn.cursor()
+             cursor.execute(sql_customer_insert_ui_query, (first_name, last_name, email_id, phone_id, created_date, modified_date))
+             self.conn.commit()
+             
+             # return sucess message
+             message = "Success: customer added successfully"
+             
+             # close cursor and connection
+             self.close_sql(cursor)
+             
+             return message
+         except Exception as e:
+             # return error message
+             error_message = "There is an error in adding customer: " + str(e)
+             
+             # close cursor and connection
+             self.close_sql(cursor)
+            
+             return error_message
+         
+            
+    # submit customer info logic
+    def add_customer(self, first_name, last_name, email, phone, created_date, modified_date):
         
         # add customer email to the database and check if added successfully
-        is_email_valid = add_customer_email(conn, cursor, email, created_date, modified_date)
+        is_email_valid = self.add_customer_email(email, created_date, modified_date)
           
         # add customer phone to the database and check if added successfully
-        is_phone_valid = add_customer_phone(conn, cursor, phone, created_date, modified_date)
+        is_phone_valid = self.add_customer_phone( phone, created_date, modified_date)
             
-        #get the email id from the database
-        email_id = get_email_id(conn, cursor, email)
+        # get the email id from the database
+        email_id = self.get_email_id (email)
         
-        #get the phone_id from the database
-        phone_id = get_phone_id(conn, cursor, phone)
+        # get the phone_id from the database
+        phone_id = self.get_phone_id(phone)
         
-        #add first name, last name, email, and phone to the database 
+        # add first name, last name, email, and phone to the database 
         if is_email_valid and is_phone_valid:
-            try:
-                sql_customer_insert_ui_query = "EXEC spCustomerTrial_InsertAll ?, ?, ?, ?, ?, ?"
-                cursor.execute(sql_customer_insert_ui_query, (first_name, last_name, email_id, phone_id, created_date, modified_date))
-                conn.commit()
-                #display UI sucess message
-                return "Success: customer added successfully"
-            except Exception as e:
-                # display UI error message
-                error_message = "There is an error in adding customer: " + str(e)
-                return error_message
+            
+            message = self.add_customer_name(first_name, last_name, email_id, phone_id, created_date, modified_date)
+            return message
         else:
                 #display err message: email input or phone input is not valid
                 return "Customer's email and/or phone is not valid"
