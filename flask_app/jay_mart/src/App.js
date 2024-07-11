b0daef1f584c6c1b39ae1041bd622312bfa8ade1
Jason Wangadi <jasonlukas.wangadi8@gmail.com>
2024-07-11 13:19:49 -0700

    fixed fetching issue and display products

--- flask_app/jay_mart/src/App.js
+++ flask_app/jay_mart/src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import {displayTable, handleOrder} from './productTable.js';
+import { useEffect } from 'react';
 
 function HomeHeaders() {
   return (
@@ -9,100 +11,34 @@ function HomeHeaders() {
   );
 }
 
- function fetchProductList() {
-  // when the products is successfully fetched, 
-  //return array as the resolved value
-  return new Promise((resolve, reject) => {
-     // request: products data from the server
-     fetch("http://localhost:5000/api/products").then(response => {
-      // convert response to json
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`)
-      }
-      return response.json();
-    })
-    .then(products => {
-      // parsed response into array
-      let productList = [];
-
-      products.forEach(product => {
-        productList.push(product)
-      })
-      
-      // return array as resolved value
-      resolve(productList);
-    })
-    .catch(error => {
-      // handle errors while fetching
-      reject(error);
-    })
-    
-  }) 
-  
-    
-}
-async function getProductList(callback) {
-  try {
-    // get productList array
-    const productList = await fetchProductList();
-    
-    // pass the array to the callback
-    callback(productList);
-
-  } catch(error) {
-    // if fetch is not successful, display an error
-    console.log("Error fetching data:", error)
-  }
- 
-}
  function ProductTable() {
-  // get the productList array
-  getProductList((productList) => {
-    console.log("ProductList array received", productList);
-  })
-  //  getProductList((productList) => {
-  //  return (
-  //   <table>
-  //     <tr>
-  //       <th>Product ID</th>
-  //       <th>Product Category</th>
-  //       <th>Product Name</th>
-  //       <th>Price</th>
-  //       <th>Quantity</th>
-  //     </tr>
-  //     <tr>
-  //       productList.
-  //       <td>1</td>
-  //       <td>Food</td>
-  //       <td>Hamburger</td>
-  //       <td>10</td>
-  //     </tr>
-  //   </table>
-  //  )
-  // })
-
- 
-
-  // display in the table
-   return (
-    <table>
-      <tr>
-        <th>Product ID</th>
-        <th>Product Category</th>
-        <th>Product Name</th>
-        <th>Price</th>
-        <th>Quantity</th>
-      </tr>
-      <tr>
-        <td>1</td>
-        <td>Food</td>
-        <td>Hamburger</td>
-        <td>10</td>
-      </tr>
-    </table>
-   )
+   // display products table
+    useEffect(() => {
+      displayTable()
+    }, [])
+    
+    return (
+      <>
+      <table id="myTable">
+        <thead>
+          <tr>
+              <th>Select</th>
+              <th>Product ID</th>
+              <th>Product Category</th>
+              <th>Product Name</th>
+              <th>Price</th>
+              <th>Quantity</th>
+          </tr>
+        </thead>
+        <tbody id="data-output">
+        </tbody>
+      </table>
+      <button type="submit" onClick={handleOrder}>Add to Cart</button>
+      </>
+    )
 
 }
+
 function Home() {
   return (
     <>
